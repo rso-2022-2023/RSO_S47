07f22df6b5ccf5a7c777561b0d4803fe3551fbd9
nikolayVv <nikolaivalvasilev@gmail.com>
2022-12-31 01:01:38 +0100

    Nastavitev Java Swing aplikacije

---
+++ QuickWheels/src/si/fri/rso/samples/quickwheels/frames/OrderDetailsFrame.java
@@ -0,0 +1,78 @@
+package si.fri.rso.samples.quickwheels.frames;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class OrderDetailsFrame extends JFrame implements ActionListener {
+    private final ImageIcon logo = new ImageIcon(System.getProperty("user.dir") + "/images/logo.PNG");
+
+    private JMenuBar menuBar;
+
+    private JMenu deliveries, orders;
+
+    private JMenuItem newDelivery, deliveryDetails, newOrder, orderDetails;
+
+    private JComboBox ordersList;
+
+    OrderDetailsFrame() {
+        this.setTitle("Quick Wheels - Order Details");
+        this.setSize(1200, 800);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setIconImage(logo.getImage());
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setLayout(null);
+        this.setupMenu();
+        this.loadOrders();
+
+        this.add(menuBar);
+        this.setJMenuBar(menuBar);
+        this.getContentPane().setBackground(new Color(0xfefeda));
+        this.setVisible(true);
+    }
+
+    private void loadOrders() {
+
+    }
+
+    private void setupMenu() {
+        menuBar = new JMenuBar();
+        deliveries = new JMenu("Deliveries");
+        orders = new JMenu("Orders");
+        newDelivery = new JMenuItem("New Delivery");
+        deliveryDetails = new JMenuItem("Delivery Details");
+        newOrder = new JMenuItem("New Order");
+        orderDetails = new JMenuItem("Order Details");
+
+        newDelivery.addActionListener(this);
+        deliveryDetails.addActionListener(this);
+        newOrder.addActionListener(this);
+        orderDetails.addActionListener(this);
+
+        menuBar.add(deliveries);
+        menuBar.add(orders);
+        deliveries.add(newDelivery);
+        deliveries.add(deliveryDetails);
+        orders.add(newOrder);
+        orders.add(orderDetails);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("New Delivery")) {
+            new NewDeliveryFrame();
+            this.dispose();
+        } else if (e.getActionCommand().equals("Delivery Details")) {
+            new DeliveryDetailsFrame();
+            this.dispose();
+        } else if (e.getActionCommand().equals("New Order")) {
+            new NewOrderFrame();
+            this.dispose();
+        } else if (e.getActionCommand().equals("Order Details")) {
+            new OrderDetailsFrame();
+            this.dispose();
+        }
+    }
+}
